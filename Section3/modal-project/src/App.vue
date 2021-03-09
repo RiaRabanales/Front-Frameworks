c3e92465a66dd01b3da633257810b0bb79dd7e06
RiaRabanales <mrabanales@cifpfbmoll.eu>
2021-03-09 10:55:45 +0100

    Hasta leccion 24

--- Section3/modal-project/src/App.vue
+++ Section3/modal-project/src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <br>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Aquí importo el componente Modal (de forma general) que uso arriba a través del objeto en la propiedad components.
+// Lo llamaré con <Modal />
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App :)'
+    }
+  },
+  methods: {
+    handleClick() {
+      // el ref funciona como handle con this.$refs.miRef
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
 }
 </script>
@@ -22,5 +38,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ Section3/modal-project/src/components/Modal.vue
@@ -0,0 +1,34 @@
+<!-- Por convención: el nombre del componente con mayúscula -->
+<template>
+  <div class="backdrop">
+    <div class="modal">
+        <h1>Modal Title</h1>
+      <p>modal content</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+h1 {
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+}
+p {
+    font-style: normal;
+}
+</style>
