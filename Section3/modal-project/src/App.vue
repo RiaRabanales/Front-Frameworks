50f661d7b625ecb87008588b8cfc0f45613bce1c
RiaRabanales <mrabanales@cifpfbmoll.eu>
2021-03-09 18:05:09 +0100

    Empiezo seccion 4

--- Section3/modal-project/src/App.vue
+++ Section3/modal-project/src/App.vue
@@ -1,33 +1,65 @@
 <template>
   <h1>{{ title }}</h1>
-  <br>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
-  <Modal />
+  <br />
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me for focus</button>
+  <br /><br />
+  <button @click="toggleModal">modal 1</button>
+  <button @click="toggleModalTwo">modal 2</button>
+
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <div v-if="showModalTwo">
+    <Modal theme="notsale" @close="toggleModalTwo">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Mi Giveaway!</h1>
+      <p>Aquí haciendo pruebas</p>
+    </Modal>
+  </div>
 </template>
 
 <script>
 // Aquí importo el componente Modal (de forma general) que uso arriba a través del objeto en la propiedad components.
 // Lo llamaré con <Modal />
-import Modal from './components/Modal.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: { Modal },
   data() {
     return {
-      title: 'My First Vue App :)'
-    }
+      title: "My First Vue App :)",
+      header: "Sign up for the Giveaway",
+      text: "esto es el texto del modal que paso desde app.vue",
+      showModal: false,
+      showModalTwo: false,
+    };
   },
   methods: {
     handleClick() {
       // el ref funciona como handle con this.$refs.miRef
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
-    }
-  }
-}
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
+
+  /* Nota: para teleport ver vid 30.
+  permite tomar un componente y transportarlo fuera.
+  ej. para pasar el modal al index:
+  <teleport to=".modalsIndex" v-if.....
+  */
 </script>
 
 <style>
@@ -43,5 +75,8 @@ h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
+}
+button {
+  margin: 0 2px;
 }
 </style>
--- Section3/modal-project/src/components/Modal.vue
+++ Section3/modal-project/src/components/Modal.vue
@@ -1,14 +1,41 @@
 <!-- Por convención: el nombre del componente con mayúscula -->
 <template>
-  <div class="backdrop">
-    <div class="modal">
-        <h1>Modal Title</h1>
-      <p>modal content</p>
+  <div class="backdrop" @click.self="closeModal">
+    <!-- click.eventModifier en este caso hace q sólo vaya el click directo, no en hijos -->
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <!-- aquí paso la clase por props.
+      también podría querer pasar las cosas por template como SLOT:
+      <modal @close="toggleModal">
+        <h1>hola</h1>
+      </modal>
+      Esto en el componente hijo se me incluiría donde pusiera el tag slot
+      El slot tb se puede pasar 'named', con <template v-slot:nombre>
+      -->
+      <slot>
+        <h1>{{ header }}</h1>
+        <p>{{ text }}</p>
+      </slot>
+      <div class="actions"> <!-- en el primer modal no aparece porque no lo tiene -->
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      // esto es un custom event: se define en el componente y escucha desde el padre component
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+
+<style>
 .backdrop {
   top: 0;
   position: fixed;
@@ -23,12 +50,38 @@
   background: white;
   border-radius: 10px;
 }
-h1 {
-    color: #03cfb4;
-    border: none;
-    padding: 0;
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
 }
-p {
-    font-style: normal;
+.modal p {
+  font-style: normal;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+  color: #333;
+}
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h1 {
+  color: white;
+}
+.modal.sale .actions {
+  color: white;
+}
+.modal.sale .actions a {
+  color: white;
 }
 </style>
