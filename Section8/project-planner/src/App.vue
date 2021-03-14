0e9730e126e032c867f2179dda2edd991d6a158a
RiaRabanales <mrabanales@cifpfbmoll.eu>
2021-03-14 14:23:05 +0100

    hasta lección 58

---
+++ Section8/project-planner/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- Para lanzar db: npx json-server --watch data/db.json -->
+  <router-view/>
+</template>
+
+<style>
+body {
+  background: #f2f2f2;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+}
+</style>
---
+++ Section8/project-planner/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data) //cuando fetcheo los datos los meto en mi variable proyectos
+      .catch(err => console.log(err.message))
+}
+}
+</script>
