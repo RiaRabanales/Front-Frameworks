5afac1dfd9515b1499ead80fd32ae85e460c57c7
RiaRabanales <mrabanales@cifpfbmoll.eu>
2021-03-15 13:07:04 +0100

    hasta lección 71

---
+++ Section9/dojo-blog/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>My name is {{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  // setup es una función propia del composition API; se lanza antes de todos los hooks
+  setup() {
+    console.log("funcion setup");
+    // las variables que declaro aquí no son reactivas (a diferencia de data()) así que no cambian en el template
+    let name = "ria";
+
+    return { name };
+  },
+};
+</script>
