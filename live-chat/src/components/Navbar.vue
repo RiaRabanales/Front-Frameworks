714d6e2d6ea87d9caaad9b7031e6cede15fa5f37
RiaRabanales <mrabanales@cifpfbmoll.eu>
2021-03-21 13:43:52 +0100

    hasta leccion 110

---
+++ live-chat/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey there... usuario</p>
+      <p class="email">Currently logged in as... email</p>
+    </div>
+    <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
