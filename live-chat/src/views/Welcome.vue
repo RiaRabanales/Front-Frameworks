20c2406b57b7a123a4a5e189428883d6fdfbc126
RiaRabanales <mrabanales@cifpfbmoll.eu>
2021-03-21 12:23:58 +0100

    hasta leccion 104

--- live-chat/src/views/Welcome.vue
+++ live-chat/src/views/Welcome.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="welcome container">
-      <p>Welcome!</p>
+    <p>Welcome!</p>
+    <div v-if="showLogin">
+      <h2>Log in</h2>
+      <LoginForm />
+      <p>No account yet? <span @click="showLogin = false">Sign up</span> instead.</p>
+    </div>
+    <div v-else>
+      <h2>Sign up</h2>
+      <SignupForm />
+      <p>Already registered? <span @click="showLogin = true">Log in</span> instead.</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import SignupForm from "../components/SignupForm.vue";
+import LoginForm from "../components/LoginForm.vue";
+import { ref } from 'vue';
 
-}
+export default {
+  components: { SignupForm, LoginForm },
+  setup() {
+    const showLogin = ref(true);
+
+    return { showLogin }
+  }
+};
 </script>
 
 <style>
 .welcome {
-    text-align: center;
-    padding: 20px 0;
+  text-align: center;
+  padding: 20px 0;
+}
+
+/* Estilos de formularios */
+.welcome form {
+  width: 300px;
+  margin: 20px auto;
+}
+.welcome label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid #eee;
+  outline: none;
+  color: #999;
+  margin: 10px auto;
+}
+.welcome span {
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+.welcome button {
+  margin: 16px auto;
 }
 </style>
