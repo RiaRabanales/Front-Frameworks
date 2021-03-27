a847d9861f0b38a66b2ba2d3ccfcbb6d7c21cc7d
RiaRabanales <mrabanales@cifpfbmoll.eu>
2021-03-27 13:42:34 +0100

    hasta lección 117

--- live-chat/src/views/Chatroom.vue
+++ live-chat/src/views/Chatroom.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
+import { watch } from 'vue';
+import { useRouter } from 'vue-router';
 import Navbar from "../components/Navbar.vue";
+import getUser from '../composables/getUser';
+import NewChatForm from '../components/NewChatForm.vue'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, NewChatForm },
+  setup() {
+    //Esto para controlar el usuario actual
+    const { user } = getUser();
+    const router = useRouter();
+
+    watch(user, () => {
+      if (!user.value) {
+        //Si no tengo usuario, redirecciono
+        router.push({ name: 'Welcome' });
+      }
+    });
+  }
 };
 </script>
 
