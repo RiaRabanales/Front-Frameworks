b3778cf403ab1e282403619ad26ab802bc56688b
Maria Rabanales <maria.rabanales@grupocmc.es>
2021-07-09 10:12:46 +0200

    Hasta lección 33; añado datepicker con rangos

--- Angular/fitness-tracker/src/app/auth/signup/signup.component.ts
+++ Angular/fitness-tracker/src/app/auth/signup/signup.component.ts
@@ -7,10 +7,16 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  maxDate: Date | undefined;
+  minDate: Date | undefined;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.maxDate = new Date();  //resuelve hoy
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+    this.minDate = new Date();
+    this.minDate.setFullYear(this.minDate.getFullYear() - 120);
   }
 
   onSubmit(form:NgForm) {
--- Angular/fitness-tracker/src/app/material.module.ts
+++ Angular/fitness-tracker/src/app/material.module.ts
@@ -4,12 +4,16 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
 
 const MAT_MODULES = [
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
 ];
 
 @NgModule({
