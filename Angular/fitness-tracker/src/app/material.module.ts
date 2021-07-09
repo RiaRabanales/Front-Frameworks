fa9c08862c409729438cfc149c69459e5b6743bb
Maria Rabanales <maria.rabanales@grupocmc.es>
2021-07-09 14:01:03 +0200

    hasta lección 50: planteo spinner

--- Angular/fitness-tracker/src/app/material.module.ts
+++ Angular/fitness-tracker/src/app/material.module.ts
@@ -10,6 +10,11 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
 
 const MAT_MODULES = [
     MatButtonModule,
@@ -21,7 +26,12 @@ const MAT_MODULES = [
     MatCheckboxModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule,
+    MatCardModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatDialogModule
 ];
 
 @NgModule({
--- Angular/fitness-tracker/src/app/training/current-training/current-training.component.ts
+++ Angular/fitness-tracker/src/app/training/current-training/current-training.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress: number = 0;
+  timer: number;
 
-  constructor() { }
+  constructor() {
+    this.timer = this.progress;
+   }
 
   ngOnInit(): void {
+    this.timer = window.setInterval(() => {
+      this.progress = this.progress + 1;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      }
+    }, 500);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
   }
 
 }
